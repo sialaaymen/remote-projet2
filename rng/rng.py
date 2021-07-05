b9c3f443a6fd29ef5a4ea1352304a75ae1a67d09
dev1 <dev1@gmail.com>
2021-07-05 13:19:23 +0100

    test rng.py

--- rng/rng.py
+++ rng/rng.py
@@ -1,2 +1,3 @@
 from flask import Flask, Responseimport osimport socketimport tim
-app = Flask(__name__)hostname = socket.gethostname()urandom = os.open("/dev/urandom", os.O_RDONLY)e
+app = Flask(__name__)hostname = socket.gethostname()urandom = os.open("/dev/urandom", os.O_RDONLY
+        @app.route("/")def index():return "RNG running on {}\n".format(hostname))e

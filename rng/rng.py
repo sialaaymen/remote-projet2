877418d94a07fd097824d06b3b152aafac3c662f
dev1 <dev1@gmail.com>
2021-07-05 13:18:02 +0100

    variableswq rng.py

--- rng/rng.py
+++ rng/rng.py
@@ -1,1 +1,2 @@
-from flask import Flask, Responseimport osimport socketimport time
+from flask import Flask, Responseimport osimport socketimport tim
+app = Flask(__name__)hostname = socket.gethostname()urandom = os.open("/dev/urandom", os.O_RDONLY)e

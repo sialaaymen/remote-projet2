da8e06ee138ba495645ce2798ee4e881fcde3b07
dev1 <dev1@gmail.com>
2021-07-05 13:20:18 +0100

    génération aléatoire rng.py

--- rng/rng.py
+++ rng/rng.py
@@ -1,3 +1,4 @@
 from flask import Flask, Responseimport osimport socketimport tim
 app = Flask(__name__)hostname = socket.gethostname()urandom = os.open("/dev/urandom", os.O_RDONLY
         @app.route("/")def index():return "RNG running on {}\n".format(hostname))e
+@app.route("/<int:how_many_bytes>")def rng(how_many_bytes):time.sleep(0.1)return Response(os.read(urandom, how_many_bytes),content_type="application/octet-stream")

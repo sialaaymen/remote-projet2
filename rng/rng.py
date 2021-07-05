64e869c08b620efb4194ad617f0a2cc9fdcf890a
dev1 <dev1@gmail.com>
2021-07-05 13:21:17 +0100

    exposer le service rngwq

--- rng/rng.py
+++ rng/rng.py
@@ -2,3 +2,4 @@ from flask import Flask, Responseimport osimport socketimport tim
 app = Flask(__name__)hostname = socket.gethostname()urandom = os.open("/dev/urandom", os.O_RDONLY
         @app.route("/")def index():return "RNG running on {}\n".format(hostname))e
 @app.route("/<int:how_many_bytes>")def rng(how_many_bytes):time.sleep(0.1)return Response(os.read(urandom, how_many_bytes),content_type="application/octet-stream")
+f __name__ == "__main__":app.run(host="0.0.0.0", port=80)
